fd5faa4f907568f4e7c6cc0129556e80eee9157c
pavandusane <dusanepv7@gmail.com>
2020-10-01 22:21:17 +0530

    Added : first name validation

--- user_reg.sh
+++ user_reg.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "***********User Registration Form***************"
+
+echo "Enter your first name : "
+read fname
+pat="^[a-zA-Z*]{3,}$"
+if [[ $fname =~ $pat ]]
+then
+  echo "Hello Mr.$fname"
+else
+  echo "Name should be begin with alphabets or having minimum length is 3"
+fi
