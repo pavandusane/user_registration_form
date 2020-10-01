2579f63ce4a38f78480b000f36d0d113e9233a7f
pavandusane <dusanepv7@gmail.com>
2020-10-01 22:28:38 +0530

    Added : Email Address Validation

--- user_reg.sh
+++ user_reg.sh
@@ -21,3 +21,12 @@ then
 else
   echo "Last name should be begin with alphabets or having minimum length is 3"
 fi
+
+echo "Enter your email id : "
+read email
+if [[ "$email" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$ ]]
+then
+    echo "Your Email Address is $email"
+else
+   echo "Please Enter Valid Email Address"
+fi
